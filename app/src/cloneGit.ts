6e19b0f21ae579bc6c0a02bf03a2b9f6e629af17
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-09-18 15:44:03 +0200

    serialize

--- app/src/cloneGit.ts
+++ app/src/cloneGit.ts
@@ -1,5 +1,27 @@
 import $ from "./lib/shell"
 import { log } from "./lib/logger"
+import path from "path"
+import interpolateTemplate from "josm-interpolate-string"
 const args = require("yargs").argv
+import InternalSerialize from "./lib/serialize"
 
-log(args)
+const cloneTemplate = {
+  ssh: "git@github.com:maximilianMairinger/$[ name ].git",
+  http: "https://github.com/maximilianMairinger/$[ name ].git"
+}
+
+
+let serialize = new InternalSerialize("config")
+serialize.read
+
+
+let repo = args._.first as string
+let dest = args._[1]
+if (!repo) {
+  repo = path.basename(path.resolve(""))
+}
+
+if (!repo.includes(".")) {
+  repo = "https://github.com"
+}
+
